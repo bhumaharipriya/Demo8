35344d3bf6572ab60188691a0518ca06bc3856c0
bhmaharipriya <bhumaharipriya25@gmail.com>
2021-11-11 16:47:17 +0530

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Demo8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 10;
+        int num2 = 20;
+
+        swap(ref num1, ref num2);
+        Console.WriteLine("num1: {0}, and num2: {1}", num1, num2);
+    }
+
+    static void swap(ref int x, ref int y)
+    {
+        int temp = x;
+        x = y;
+        y = temp;
+        }
+    }
+}
